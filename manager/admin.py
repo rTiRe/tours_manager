a42892f883a7747ccc0fc18f778baa4c1761eaf8
Артём Тюрин <zientenin@mail.ru>
2024-05-03 09:16:26 +0300

    Add admin-panel

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Agency, Tour, City, TourCity
 
-# Register your models here.
+
+class TourCityInline(admin.TabularInline):
+    model = TourCity
+    extra = 0
+
+
+@admin.register(Agency)
+class AgencyAdmin(admin.ModelAdmin):
+    model = Agency
+    list_display = ['name', 'phone_number', 'rating']
+    search_fields = ['name', 'phone_number']
+    list_filter = ('name', 'phone_number', 'rating')
+
+
+@admin.register(Tour)
+class TourAdmin(admin.ModelAdmin):
+    model = Tour
+    list_display = ['name', 'description']
+    search_fields = ['name', 'description']
+    list_filter = ('name',)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    model = City
+    list_display = ['name', 'country']
+    search_fields = ['name', 'country']
+    inlines = (TourCityInline,)
+
+
+@admin.register(TourCity)
+class TourCityAdmin(admin.ModelAdmin):
+    model = TourCity
