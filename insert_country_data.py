341d495c3e9e1f19ad66f1a30a05f4fa2c8bbd80
Артём Тюрин <zientenin@mail.ru>
2024-05-04 02:12:26 +0300

    Add script for insert country names

---
+++ insert_country_data.py
@@ -0,0 +1,34 @@
+from psycopg import connect
+from dotenv import load_dotenv
+from os import getenv
+from random import randint
+
+load_dotenv()
+
+creds = (
+    ('host', 'PG_HOST'),
+    ('port', 'PG_PORT'),
+    ('user', 'PG_USER'),
+    ('password', 'PG_PASSWORD'),
+    ('dbname', 'PG_DBNAME')
+)
+
+creds = {var: getenv(env_var) for var, env_var in creds}
+
+connection = connect(**creds)
+cursor = connection.cursor()
+
+COUNTRIES = []
+
+import csv
+file_path = 'countries.csv'
+with open(file_path, 'r', encoding='utf-8') as countries_csv:
+    reader = csv.reader(countries_csv)
+    COUNTRIES.extend(next(reader))
+
+insert_link = 'INSERT INTO tours_data.country (name) VALUES (%s)'
+
+for country in COUNTRIES:
+    cursor.execute(insert_link, (country))
+
+connection.commit()
