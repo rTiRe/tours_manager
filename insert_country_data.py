4807547039c9e0e925a75cb38bfeb104a0d5be9a
Артём Тюрин <zientenin@mail.ru>
2024-05-04 02:26:28 +0300

    Fix insert script

--- insert_country_data.py
+++ insert_country_data.py
@@ -2,6 +2,7 @@ from psycopg import connect
 from dotenv import load_dotenv
 from os import getenv
 from random import randint
+from uuid import uuid4
 
 load_dotenv()
 
@@ -26,9 +27,9 @@ with open(file_path, 'r', encoding='utf-8') as countries_csv:
     reader = csv.reader(countries_csv)
     COUNTRIES.extend(next(reader))
 
-insert_link = 'INSERT INTO tours_data.country (name) VALUES (%s)'
+insert_link = 'INSERT INTO tours_data.country (id, name) VALUES (%s, %s)'
 
 for country in COUNTRIES:
-    cursor.execute(insert_link, (country))
+    cursor.execute(insert_link, (uuid4(), country))
 
 connection.commit()
