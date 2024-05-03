01e326d1235a3067685164af5f587443f3ba6d17
Артём Тюрин <zientenin@mail.ru>
2024-05-03 03:16:35 +0300

    Add NameMixin to models

--- manager/models.py
+++ manager/models.py
@@ -4,11 +4,26 @@ from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
 
 
+NAME_MAX_LEN = 255
+
+
 class UUIDMixin(models.Model):
     id = models.UUIDField(
         primary_key=True,
         default=uuid4,
         editable=False
+    )
+
+    class Meta:
+        abstract = True
+
+
+class NameMixin(models.Model):
+    name = models.CharField(
+        verbose_name = _('name'),
+        null = False,
+        blank = False,
+        max_length = NAME_MAX_LEN
     )
 
     class Meta:
