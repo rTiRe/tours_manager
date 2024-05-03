20ca7df2b5cfb69d66b7e93660dfee25e2d42e8f
Артём Тюрин <zientenin@mail.ru>
2024-05-04 02:56:12 +0300

    Add ReviewForm with min-max values for rating

--- manager/forms.py
+++ manager/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Address
+from .models import Address, Review
 
 class AddressForm(forms.ModelForm):
     class Meta:
@@ -9,4 +9,13 @@ class AddressForm(forms.ModelForm):
             'entrance_number': forms.NumberInput(attrs={'min': 1, 'step': 1}),
             'floor': forms.NumberInput(attrs={'min': -1, 'step': 1}),
             'flat_number': forms.NumberInput(attrs={'min': 1, 'step': 1}),
+        }
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        widgets = {
+            'rating': forms.NumberInput(attrs={'min': 1, 'max': 5, 'step': 0.1}),
         }
