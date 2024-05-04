d8aea18f5baf7bf3bc13d9d9ac7d373a25ab41e1
Артём Тюрин <zientenin@mail.ru>
2024-05-04 03:15:59 +0300

    Delete validator field from City in migration 0002

--- manager/migrations/0002_city_tourcity_city_tours_tour_cities_and_more.py
+++ manager/migrations/0002_city_tourcity_city_tours_tour_cities_and_more.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
                 ('name', models.CharField(max_length=255, verbose_name='name')),
-                ('country', models.CharField(max_length=255, validators=[manager.models.country_validator], verbose_name='country')),
+                ('country', models.CharField(max_length=255, verbose_name='country')),
             ],
             options={
                 'verbose_name': 'city',
