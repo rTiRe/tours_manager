136f5e1500eada5bc6b8244f06ed91684730e055
Артём Тюрин <zientenin@mail.ru>
2024-05-09 02:44:10 +0300

    Replace JsonResponse to rest_framework Response

--- manager/views.py
+++ manager/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import JsonResponse, QueryDict
+from django.http import QueryDict
 from django.core import exceptions
-from rest_framework import viewsets, permissions, authentication, request
+from rest_framework import viewsets, permissions, authentication, request, status
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .models import Agency, Tour, City, Country, Review, Address, Account
 from .serializers import AgencySerializer, TourSerializer, CitySerializer, \
@@ -9,9 +10,11 @@ from .serializers import AgencySerializer, TourSerializer, CitySerializer, \
 from django.utils.translation import gettext_lazy as _
 from typing import Any
 import json
-from .validators import password_validator, username_validator, user_fields_validator, name_validator
+from .validators import password_validator, username_validator, \
+    user_fields_validator, name_validator
 import re
-import email_validate
+from email_validate import validate
+
 
 def index(request):
     return render(
@@ -25,9 +28,10 @@ class CustomViewSetPermission(permissions.BasePermission):
     _unsafe_methods = 'PATCH', 'POST', 'PUT', 'DELETE'
 
     def has_permission(self, request, _):
-        if request.method in self._safe_methods and (request.user and request.user.is_authenticated):
+        user = request.user
+        if request.method in self._safe_methods and (user and user.is_authenticated):
             return True
-        if request.method in self._unsafe_methods and (request.user and request.user.is_superuser):
+        if request.method in self._unsafe_methods and (user and user.is_superuser):
             return True
         return False
 
@@ -55,7 +59,7 @@ class AccountViewSet(viewsets.ModelViewSet):
     permission_classes = [CustomViewSetPermission]
     authentication_classes = [authentication.TokenAuthentication]
 
-    def __create_user(self, data: request.Empty | dict | QueryDict | Any) -> User | JsonResponse:
+    def __create_user(self, data: request.Empty | dict | QueryDict | Any) -> User | Response:
         try:
             user_fields_validator(data)
         except exceptions.ValidationError as e:
@@ -63,14 +67,15 @@ class AccountViewSet(viewsets.ModelViewSet):
             replaceable = json.loads(str(e))[0]
             replacement = '"\\g<1>": "\\g<2>"'
             replaced = re.sub(rule, replacement, replaceable, 0, re.MULTILINE)
-            return JsonResponse(json.loads(replaced))
-        if not email_validate.validate(data['email'], check_blacklist=False, check_dns=False, check_smtp=False):
-            return JsonResponse({'email': _('Incorrect email address.')})
+            return Response(json.loads(replaced), status=status.HTTP_400_BAD_REQUEST)
+        if not validate(data['email'], check_blacklist=False, check_dns=False, check_smtp=False):
+            response_json = {'email': _('Incorrect email address.')}
+            return Response(response_json, status=status.HTTP_400_BAD_REQUEST)
         username = data['username']
         try:
             username_validator(username)
         except (exceptions.ValidationError, ValueError) as e:
-            return JsonResponse({'username': str(e)})
+            return Response({'username': str(e)}, status=status.HTTP_400_BAD_REQUEST)
         password = data['password']
         password2 = data['password2']
         try:
@@ -85,23 +90,23 @@ class AccountViewSet(viewsets.ModelViewSet):
                 e = json.loads(replaced)
             except json.decoder.JSONDecodeError:
                 e = json.loads(f'"{replaced}"')
-            return JsonResponse({'password': e})
+            return Response({'password': e}, status=status.HTTP_400_BAD_REQUEST)
         name_validator(data['first_name'], 'first')
         name_validator(data['last_name'], 'last')
         return User.objects.create_user(**data)
 
-    def create(self, request: request.Request) -> JsonResponse:
+    def create(self, request: request.Request) -> Response:
         data = request.data
         try:
             is_agency = data.pop('is_agency')
         except KeyError:
             is_agency = False
         user = self.__create_user(data)
-        if isinstance(user, JsonResponse):
+        if isinstance(user, Response):
             return user
         Account.objects.create(account=user, is_agency=is_agency)
         del data['password']
         data['is_agency'] = is_agency
         print(data)
-        return JsonResponse(data)
+        return Response(data, status=status.HTTP_201_CREATED)
 
