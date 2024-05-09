2c3453495abc27c028814501cdee46cbc71051a2
Артём Тюрин <zientenin@mail.ru>
2024-05-09 04:33:35 +0300

    Refactor __create_user function

--- manager/views.py
+++ manager/views.py
@@ -8,9 +8,9 @@ from django.http import QueryDict
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
 from email_validate import validate
-from rest_framework import (authentication, permissions, request, status,
-                            viewsets)
+from rest_framework import authentication, permissions, status, viewsets
 from rest_framework.response import Response
+from rest_framework.request import Request, Empty
 
 from .models import Account, Address, Agency, City, Country, Review, Tour
 from .serializers import (AccountSerializer, AddressSerializer,
@@ -71,7 +71,7 @@ class AccountViewSet(viewsets.ModelViewSet):
     ) -> str:
         return re.sub(rule, replacement, replaceable, 0, re.MULTILINE)
 
-    def __name_validator(self, data: request.Empty | dict | QueryDict | Any) -> None | Response:
+    def __name_validator(self, data: Empty | dict | QueryDict | Any) -> None | Response:
         for name in ('first', 'last'):
             try:
                 name_validator(data[f'{name}_name'], name)
@@ -79,25 +79,7 @@ class AccountViewSet(viewsets.ModelViewSet):
                 replaced = json.loads(self.replace_single_quote(str(e)))[0]
                 return Response({f'{name}_name': replaced}, status=status.HTTP_400_BAD_REQUEST)
 
-    def __create_user(self, data: request.Empty | dict | QueryDict | Any) -> User | Response:
-        try:
-            user_fields_validator(data)
-        except exceptions.ValidationError as e:
-            replaced = self.replace_single_quote(
-                json.loads(str(e))[0],
-                '"\\g<1>": "\\g<2>"',
-                r'\'(\w+)\': \'([\w\s]*)\''
-            )
-            return Response(json.loads(replaced), status=status.HTTP_400_BAD_REQUEST)
-        if not validate(data['email'], check_blacklist=False, check_dns=False, check_smtp=False):
-            response_json = {'email': _('Incorrect email address.')}
-            return Response(response_json, status=status.HTTP_400_BAD_REQUEST)
-        username = data['username']
-        try:
-            username_validator(username)
-        except (exceptions.ValidationError, ValueError, TypeError) as e:
-            replaced = json.loads(self.replace_single_quote(str(e)))[0]
-            return Response({'username': replaced}, status=status.HTTP_400_BAD_REQUEST)
+    def __password_validator(self, data: Empty | dict | QueryDict | Any) -> None | Response:
         password = data['password']
         password2 = data.pop('password2')
         try:
@@ -111,12 +93,46 @@ class AccountViewSet(viewsets.ModelViewSet):
             except json.decoder.JSONDecodeError:
                 e = json.loads(f'"{replaced}"')
             return Response({'password': e}, status=status.HTTP_400_BAD_REQUEST)
-        name_validation_result = self.__name_validator(data)
-        if name_validation_result:
-            return name_validation_result
+
+    def __username_validator(self, data: Empty | dict | QueryDict | Any) -> None | Response:
+        username = data['username']
+        try:
+            username_validator(username)
+        except (exceptions.ValidationError, ValueError, TypeError) as e:
+            replaced = json.loads(self.replace_single_quote(str(e)))[0]
+            return Response({'username': replaced}, status=status.HTTP_400_BAD_REQUEST)
+
+    def __email_validator(self, data: Empty | dict | QueryDict | Any) -> None | Response:
+        if not validate(data['email'], check_blacklist=False, check_dns=False, check_smtp=False):
+            response_json = {'email': _('Incorrect email address.')}
+            return Response(response_json, status=status.HTTP_400_BAD_REQUEST)
+
+    def __fields_validator(self, data: Empty | dict | QueryDict | Any) -> None | Response:
+        try:
+            user_fields_validator(data)
+        except exceptions.ValidationError as e:
+            replaced = self.replace_single_quote(
+                json.loads(str(e))[0],
+                '"\\g<1>": "\\g<2>"',
+                r'\'(\w+)\': \'([\w\s]*)\''
+            )
+            return Response(json.loads(replaced), status=status.HTTP_400_BAD_REQUEST)
+
+    def __create_user(self, data: Empty | dict | QueryDict | Any) -> User | Response:
+        validators = [
+            self.__fields_validator,
+            self.__email_validator,
+            self.__username_validator,
+            self.__password_validator,
+            self.__name_validator,
+        ]
+        for validator in validators:
+            validation_result = validator(data)
+            if validation_result:
+                return validation_result
         return User.objects.create_user(**data)
 
-    def create(self, request: request.Request) -> Response:
+    def create(self, request: Request) -> Response:
         data = request.data
         try:
             is_agency = data.pop('is_agency')
@@ -130,7 +146,7 @@ class AccountViewSet(viewsets.ModelViewSet):
         data['is_agency'] = is_agency
         return Response(data, status=status.HTTP_201_CREATED)
 
-    def destroy(self, request: request.Request, *args, **kwargs) -> Response:
+    def destroy(self, request: Request, *args, **kwargs) -> Response:
         self.get_object().account.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
