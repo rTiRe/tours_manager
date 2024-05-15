c169c5180d90d1347252cdd3aabec92f306ac3e9
Артём Тюрин <zientenin@mail.ru>
2024-05-15 06:57:03 +0300

    views.py flake8

--- manager/views.py
+++ manager/views.py
@@ -1,13 +1,27 @@
+"""Module with page views."""
+
+from typing import Any
+
+from django.db.models import Model
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.utils.translation import gettext_lazy as _
 from rest_framework import authentication, permissions, viewsets
+from rest_framework.serializers import ModelSerializer
 
 from .models import Address, Agency, Review, Tour
 from .serializers import (AddressSerializer, AgencySerializer,
                           ReviewSerializer, TourSerializer)
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
+    """Index page view.
+
+    Args:
+        request: HttpRequest - request from user..
+
+    Returns:
+        HttpResponse: response from server.
+    """
     return render(
         request,
         'index.html',
@@ -15,19 +29,37 @@ def index(request):
 
 
 class CustomViewSetPermission(permissions.BasePermission):
+    """Custom permission for viewset."""
+
     _safe_methods = 'GET', 'HEAD', 'OPTIONS', 'TRACE'
     _unsafe_methods = 'PATCH', 'POST', 'PUT', 'DELETE'
 
-    def has_permission(self, request, _):
+    def has_permission(self, request: HttpRequest, _: Any) -> bool:
+        """Check user permissions.
+
+        Args:
+            request: HttpRequest - request from user.
+            _: Any - other data.
+
+        Returns:
+            bool: True if user has required permissions.
+        """
         user = request.user
         if request.method in self._safe_methods and (user and user.is_authenticated):
             return True
-        if request.method in self._unsafe_methods and (user and user.is_superuser):
-            return True
-        return False
+        return request.method in self._unsafe_methods and (user and user.is_superuser)
 
 
-def create_viewset(model_class, serializer):
+def create_viewset(model_class: Model, serializer: ModelSerializer) -> viewsets.ModelViewSet:
+    """Viewset decorator.
+
+    Args:
+        model_class: Model - model for generate viewset.
+        serializer: ModelSerializer - serializer for model.
+
+    Returns:
+        ModelViewSet: model viewset.
+    """
     class CustomViewSet(viewsets.ModelViewSet):
         serializer_class = serializer
         queryset = model_class.objects.all()
