999c41813a680b2d641d6c6cad95e3b3b98723f5
Артём Тюрин <zientenin@mail.ru>
2024-05-15 05:08:44 +0300

    Runner.py flake8

--- tests/runner.py
+++ tests/runner.py
@@ -1,16 +1,36 @@
+"""Database setup."""
+
+from types import MethodType
 from typing import Any
+
+from django.db import connections
 from django.db.backends.base.base import BaseDatabaseWrapper
 from django.test.runner import DiscoverRunner
-from django.db import connections
-from types import MethodType
 
 
 def prepare_db(self):
+    """Create database schema.
+
+    Args:
+        self: type - class object.
+
+    """
     self.connect()
     self.connection.cursor().execute('CREATE SCHEMA IF NOT EXISTS tours_data;')
 
+
 class PostgresSchemaRunner(DiscoverRunner):
+    """Run table schema."""
+
     def setup_databases(self, **kwargs: Any) -> list[tuple[BaseDatabaseWrapper, str, bool]]:
+        """Pre-setup database for work.
+
+        Args:
+            kwargs: Any - key word arguments.
+
+        Returns:
+            list[tuple[BaseDatabaseWrapper, str, bool]]: setupped database.
+        """
         for conn_name in connections:
             connection = connections[conn_name]
             connection.prepare_database = MethodType(prepare_db, connection)
