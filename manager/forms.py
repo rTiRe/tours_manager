14a298333604fd3a27fb7d7861f282c9435b687d
Артём Тюрин <zientenin@mail.ru>
2024-05-15 05:28:07 +0300

    forms.py flake8

--- manager/forms.py
+++ manager/forms.py
@@ -1,21 +1,37 @@
+"""Classes with changed forms."""
+
 from django import forms
+
 from .models import Address, Review
 
+name_min = 'min'
+name_max = 'max'
+name_step = 'step'
+
+
 class AddressForm(forms.ModelForm):
+    """Class for address form."""
+
     class Meta:
+        """Form meta class with settings."""
+
         model = Address
         fields = '__all__'
         widgets = {
-            'entrance_number': forms.NumberInput(attrs={'min': 1, 'step': 1}),
-            'floor': forms.NumberInput(attrs={'min': -1, 'step': 1}),
-            'flat_number': forms.NumberInput(attrs={'min': 1, 'step': 1}),
+            'entrance_number': forms.NumberInput(attrs={name_min: 1, name_step: 1}),
+            'floor': forms.NumberInput(attrs={name_min: -1, name_step: 1}),
+            'flat_number': forms.NumberInput(attrs={name_min: 1, name_step: 1}),
         }
 
 
 class ReviewForm(forms.ModelForm):
+    """Class for review form."""
+
     class Meta:
+        """Form meta class with settings."""
+
         model = Review
         fields = '__all__'
         widgets = {
-            'rating': forms.NumberInput(attrs={'min': 1, 'max': 5, 'step': 0.1}),
+            'rating': forms.NumberInput(attrs={name_min: 1, name_max: 5, name_step: 0.1}),
         }
