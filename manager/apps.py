f09a32fdb6fecadb24e24ad2b9ddecf9a742456d
Артём Тюрин <zientenin@mail.ru>
2024-05-15 05:22:34 +0300

    apps.py flake8

--- manager/apps.py
+++ manager/apps.py
@@ -1,6 +1,10 @@
+"""Apps config."""
+
 from django.apps import AppConfig
 
 
 class ManagerConfig(AppConfig):
+    """Setup config."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'manager'
