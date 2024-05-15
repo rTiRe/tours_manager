25c7e8a75c824c0c7655d850fed8daeddca8425d
Артём Тюрин <zientenin@mail.ru>
2024-05-15 04:43:17 +0300

    Delete useless urls

--- manager/urls.py
+++ manager/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AgencyViewSet, TourViewSet, CityViewSet, CountryViewSet, \
-    ReviewViewSet, AddressViewSet, AccountViewSet
+from .views import AgencyViewSet, TourViewSet, \
+    ReviewViewSet, AddressViewSet
 
 router = DefaultRouter()
 router.register(r'agencies', AgencyViewSet)
 router.register(r'tours', TourViewSet)
-router.register(r'cities', CityViewSet)
-router.register(r'countries', CountryViewSet)
 router.register(r'reviews', ReviewViewSet)
 router.register(r'addresses', AddressViewSet)
-router.register(r'accounts', AccountViewSet)
 
 from . import views
 
