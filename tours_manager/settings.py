f3b16f432055e1e0103ab52d3a47d8dda585ee9b
Артём Тюрин <zientenin@mail.ru>
2024-05-16 19:03:54 +0300

    setup minio storage

--- tours_manager/settings.py
+++ tours_manager/settings.py
@@ -35,6 +35,24 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+if DEBUG:
+    AWS_ACCESS_KEY_ID = getenv('MINIO_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = getenv('MINIO_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = getenv('MINIO_STORAGE_BUCKET_NAME')
+    AWS_S3_ENDPOINT_URL = getenv('MINIO_API')
+    AWS_S3_USE_SSL = False
+
+MINIO_ENDPOINT = 'localhost:9000'
+MINIO_ACCESS_KEY = getenv('MINIO_ACCESS_KEY_ID')
+MINIO_SECRET_KEY = getenv('MINIO_SECRET_ACCESS_KEY')
+MINIO_USE_HTTPS = False
+MINIO_CONSISTENCY_CHECK_ON_START = True
+MINIO_PRIVATE_BUCKETS = []
+MINIO_PUBLIC_BUCKETS = [
+    'static',
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -49,6 +67,8 @@ INSTALLED_APPS = [
     'manager',
     'django_extensions',
     'django.contrib.gis',
+    'storages',
+    'django_minio_backend',
 ]
 
 REST_FRAMEWORK = {
