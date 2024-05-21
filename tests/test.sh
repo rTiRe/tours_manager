b993a2185b35882e7843662d5c721c6317d41ec5
Артём Тюрин <zientenin@mail.ru>
2024-05-21 11:12:30 +0300

    Update exports for tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+export DJANGO_SECRET_KEY = "django-insecure-pt4!7*d%-dn)&lx6@598y(1!!rljgaieunnwk!n)-#r^ew(dcb"
 export PG_HOST=127.0.0.1
 export PG_PORT=5432
 export PG_USER=test
 export PG_PASSWORD=test
 export PG_DBNAME=test
-export MINIO_ACCESS_KEY_ID=user
-export MINIO_SECRET_ACCESS_KEY=password
+export MINIO_ACCESS_KEY_ID=ULk99g9UwyucWln57ypo
+export MINIO_SECRET_ACCESS_KEY=dGKHOvOO9166VEMOO0tSRdUVVUcm5odgYgB2Yfjs
 export MINIO_STORAGE_BUCKET_NAME=static
 export MINIO_API=http://localhost:9000
 export MINIO_CONSISTENCY_CHECK_ON_START=False
