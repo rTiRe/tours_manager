fcc81deb3dd878186c18ab70aeb12727f1a29fda
Артём Тюрин <zientenin@mail.ru>
2024-05-22 16:36:03 +0300

    Fix minio workflows

--- tests/test.sh
+++ tests/test.sh
@@ -9,6 +9,6 @@ export MINIO_ACCESS_KEY_ID=ULk99g9UwyucWln57ypo
 export MINIO_SECRET_ACCESS_KEY=dGKHOvOO9166VEMOO0tSRdUVVUcm5odgYgB2Yfjs
 export MINIO_STORAGE_BUCKET_NAME=static
 export MINIO_API=http://localhost:9000
-export MINIO_CONSISTENCY_CHECK_ON_START=False
+export MINIO_CHECK_ON_START=False
 
 python3 manage.py test --verbosity=3 $1
