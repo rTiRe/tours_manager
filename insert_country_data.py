240f8f11c9a59c520dcb158a8fc8cba610f394d0
Артём Тюрин <zientenin@mail.ru>
2024-05-22 10:23:25 +0300

    insert_country flake8

--- insert_country_data.py
+++ insert_country_data.py
@@ -1,8 +1,11 @@
-from psycopg import connect
-from dotenv import load_dotenv
+"""Module for insert countries names into table."""
+
+import csv
 from os import getenv
-from random import randint
 from uuid import uuid4
+
+from dotenv import load_dotenv
+from psycopg import Connection, connect
 
 load_dotenv()
 
@@ -11,25 +14,24 @@ creds = (
     ('port', 'PG_PORT'),
     ('user', 'PG_USER'),
     ('password', 'PG_PASSWORD'),
-    ('dbname', 'PG_DBNAME')
+    ('dbname', 'PG_DBNAME'),
 )
 
-creds = {var: getenv(env_var) for var, env_var in creds}
+creds = {var_name: getenv(env_var) for var_name, env_var in creds}
 
-connection = connect(**creds)
+connection: Connection = connect(**creds)
 cursor = connection.cursor()
 
-COUNTRIES = []
+countries = []
 
-import csv
 file_path = 'countries.csv'
 with open(file_path, 'r', encoding='utf-8') as countries_csv:
     reader = csv.reader(countries_csv)
-    COUNTRIES.extend(next(reader))
+    countries.extend(next(reader))
 
-insert_link = 'INSERT INTO tours_data.country (id, name) VALUES (%s, %s)'
+insert_link = 'INSERT INTO tours_data.country (id, name) VALUES ({0}, {1})'
 
-for country in COUNTRIES:
-    cursor.execute(insert_link, (uuid4(), country))
+for country in countries:
+    cursor.execute(insert_link.format(uuid4(), country))
 
 connection.commit()
