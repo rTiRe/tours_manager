ae211991b6f25e2514d655ce978382f3ea4b3b8f
Артём Тюрин <zientenin@mail.ru>
2024-05-23 17:34:45 +0300

    Add templatetags

--- tours_manager/settings.py
+++ tours_manager/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'storages',
     'django_minio_backend',
+    'manager.templatetags.template_filters'
 ]
 
 REST_FRAMEWORK = {
@@ -166,6 +167,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
 
 LOACLE_PATH = 'manager/locale'
 
