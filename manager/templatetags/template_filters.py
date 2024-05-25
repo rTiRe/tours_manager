12766598638c4e7045298458831d9574d1c5b33c
Артём Тюрин <zientenin@mail.ru>
2024-05-25 20:50:27 +0300

    Fix zero division error if agency has zero tours

--- manager/templatetags/template_filters.py
+++ manager/templatetags/template_filters.py
@@ -23,4 +23,6 @@ def get_avg(int_list):
     if isinstance(int_list, dict):
         int_list = [dict_value for dict_value in int_list.values()]
     int_list = list(filter(lambda number: number != 0, int_list))
-    return sum(int_list) / len(int_list)
+    if int_list:
+        return sum(int_list) / len(int_list)
+    return 0
